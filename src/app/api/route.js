63c771978caf73895290b00e18e23e30a6635210
Heavy108 <Heavy1089@gmail.com>
2024-08-01 01:28:52 +0530

    alby chat app

---
+++ src/app/api/route.js
@@ -0,0 +1,14 @@
+//api/route.js
+import Ably from 'ably';
+
+
+export const revalidate = 0;
+
+export async function GET(request) {
+  const client = new Ably.Rest(process.env.ABLY_API_KEY);
+  const tokenRequestData = await client.auth.createTokenRequest({
+    clientId: 'ably-nextjs-demo',
+  });
+  console.log(`Request: ${JSON.stringify(tokenRequestData)}`);
+  return Response.json(tokenRequestData);
+}
